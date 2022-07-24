a53be0e1990d79b703bdc51adc53ac93f3dce16e
likycs <85971019+likycs@users.noreply.github.com>
2022-07-24 16:14:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ app.post('/', (req, res) => {
 
     if (req.body.convertFrom == "lbs") {
         num = (num * 0.45359237)
+        num = num.toFixed(1)
         res.json({converted: num})
     } else {
         num = (num * 2.2)
+        num = num.toFxied(1)
         res.json({converted: num})
     };
 })
