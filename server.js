378970706ac26e21e2b95a1dd05ca9ae9c915f91
likycs <85971019+likycs@users.noreply.github.com>
2022-07-24 16:43:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,17 +11,16 @@ app.post('/', (req, res) => {
         res.json({Error:'Invalid input for number'})
     }; 
 
-
     let num = req.body.number;
 
 
     if (req.body.convertFrom == 'lbs') {
         num = (num * 0.45359237)
         num = num.toFixed(1)
-        res.json({converted: num})
+        res.json({convertedTo: "kg", number: num})
     } else if (req.body.convertFrom == 'kg') {
         num = (num * 2.2)
-        res.json({converted: num})
+        res.json({convertedTo: "lbs", number: num})
     } else {
         res.json({Error:"Conversion format not supported"})
     };
