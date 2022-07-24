9dabcc5adfc18d21c4af0dc05f2fd7c60b4c871a
likycs <85971019+likycs@users.noreply.github.com>
2022-07-24 16:09:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send('testing 123')
 })
 
 app.post('/', (req, res) => {
-    if (typeof req.body.number != 'number') {
-        res.send('Invalid input for number')
+    if (typeof req.body.number !== 'number') {
+        res.json({Error:'Invalid input for number'})
     };
 
     let num = req.body.number;
 
     if (req.body.convertFrom == "lbs") {
         num = (num * 0.45359237)
-        res.json({converted: num })
+        res.json({converted: num})
     } else {
         num = (num * 2.2)
-        res.json({ converted: num })
+        res.json({converted: num})
     };
 })
 
