03b8d3b2bff3e778437a7606dccb76983d1ff5b4
likycs <85971019+likycs@users.noreply.github.com>
2022-07-24 16:41:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.post('/', (req, res) => {
     if (typeof req.body.number !== 'number') {
         res.json({Error:'Invalid input for number'})
     }; 
-    
-    if (req.body.convertFrom !== 'lbs' || req.body.convertFrom !== 'kg'){
-        res.json({Error: 'Conversion format not supported'})
-    };
+
 
     let num = req.body.number;
 
@@ -21,11 +18,13 @@ app.post('/', (req, res) => {
     if (req.body.convertFrom == 'lbs') {
         num = (num * 0.45359237)
         num = num.toFixed(1)
-        res.json({converted: num})};
-
-    if (req.body.convertFrom == 'kg') {
+        res.json({converted: num})
+    } else if (req.body.convertFrom == 'kg') {
         num = (num * 2.2)
-        res.json({converted: num})};
+        res.json({converted: num})
+    } else {
+        res.json({Error:"Conversion format not supported"})
+    };
 })
 
 
